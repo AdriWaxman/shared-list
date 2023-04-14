028c8a5223a64eac955b4d846634304ed59485fd
AdriWaxman <adrimpartner@gmail.com>
2023-04-14 19:53:51 +0200

    Fix: tailwind and postcss config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,17 @@
-module.exports = {
-  content: ['./src/**/*.{js,ts,jsx,tsx}'],
-  plugins: [require('daisyui')],
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: ["emerald", "dark"],
+  },
+  plugins: [
+    require('daisyui'),
+  ],
 }
+
